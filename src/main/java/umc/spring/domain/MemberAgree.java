1c912e2f3e1fb47e5f585b97013a076a8ea984a6
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-19 23:20:40 +0900

    feat: mapping table 구성

--- src/main/java/umc/spring/domain/MemberAgree.java
+++ src/main/java/umc/spring/domain/MemberAgree.java
@@ -1,2 +1,23 @@
-package umc.spring.domain;public class MemberAgree {
+package umc.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberAgree extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/umc/spring/domain/MemberMission.java
+++ src/main/java/umc/spring/domain/MemberMission.java
@@ -1,2 +1,29 @@
-package umc.spring.domain;public class MemberMission {
+package umc.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.spring.domain.enums.MissionStatus;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
 }
