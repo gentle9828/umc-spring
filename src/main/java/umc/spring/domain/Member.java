c6fc30290603b4ad13d896094fdf7ef2fe1ebcd3
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-19 21:14:10 +0900

    feat(Member): Member entity 구성

---
+++ src/main/java/umc/spring/domain/Member.java
@@ -0,0 +1,39 @@
+package umc.spring.domain;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String specAddress;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+
+}
