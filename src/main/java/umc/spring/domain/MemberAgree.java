b81d72fc87dbbee26eb292b4d022b7e48dfafb0d
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-19 23:22:18 +0900

    feat(MemberAgree): MemberAgree 연관 관계 매핑

--- src/main/java/umc/spring/domain/MemberAgree.java
+++ src/main/java/umc/spring/domain/MemberAgree.java
@@ -1,9 +1,12 @@
 package umc.spring.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,4 +23,12 @@ public class MemberAgree extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id")
+    private Terms terms;
 }
